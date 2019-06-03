ceab066dffad49e787bf1daaebf9228310e10edc
Sanusi Yusuf Korede <sanusikorede@gmail.com>
2019-06-03 14:09:30 +0100

    create pokesearch background

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Platform, ImageBackground } from 'react-native';
+import { Button } from 'native-base';
+
+var myBackground = require('./assets/landing.jpg');
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <ImageBackground source={myBackground} style={styles.myBackground}>
+          <View style={styles.viewStyle}>
+            <Text style={styles.titleStyle}>Welcome to PokeSearch</Text>
+            <Button
+              block={true}
+              tyle={styles.buttonStyle}
+              onPress={()=>{}}>
+                <Text style={styles.buttonText}>Start searching</Text>
+              </Button>
+          </View> 
+        </ImageBackground>
+      </View> 
     );
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === "android" ? 24 : 0,
   },
-});
+  viewStyle: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  myBackground: {width: '100%', height: '100%'},
+  titleStyle: {
+    fontSize: 30, 
+    color: 'blue',
+    alignItems: 'center'
+
+  },
+  buttonStyle: {margin: 10 },
+  buttonText: {color: 'white'}
+};
