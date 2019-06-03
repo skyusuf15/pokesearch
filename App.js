4701d9a12e51f3db1ad7fe28d41a4a1da4350ed6
Sanusi Yusuf Korede <sanusikorede@gmail.com>
2019-06-03 16:17:41 +0100

    create a landing page component

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import { Text, View, Platform, ImageBackground } from 'react-native';
-import { Button } from 'native-base';
-
-var myBackground = require('./assets/landing.jpg');
+import { View, Platform } from 'react-native';
+import Landing from './src/Landing';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ImageBackground source={myBackground} style={styles.myBackground}>
-          <View style={styles.viewStyle}>
-            <Text style={styles.titleStyle}>Welcome to PokeSearch</Text>
-            <Button
-              block={true}
-              tyle={styles.buttonStyle}
-              onPress={()=>{}}>
-                <Text style={styles.buttonText}>Start searching</Text>
-              </Button>
-          </View> 
-        </ImageBackground>
+        <Landing />
       </View> 
     );
   }
@@ -28,20 +16,5 @@ const styles = {
   container: {
     flex: 1,
     marginTop: Platform.OS === "android" ? 24 : 0,
-  },
-  viewStyle: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  myBackground: {width: '100%', height: '100%'},
-  titleStyle: {
-    fontSize: 30, 
-    color: 'blue',
-    alignItems: 'center'
-
-  },
-  buttonStyle: {margin: 10 },
-  buttonText: {color: 'white'}
+  }
 };
---
+++ src/Landing.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {View,Text, ImageBackground } from 'react-native';
+import { Button } from 'native-base';
+
+var myBackground = require('../assets/landing.jpg');
+ 
+class Landing extends React.Component {
+    render() {
+        return ( 
+            <View>
+                <ImageBackground source={myBackground} style={styles.myBackground}>
+                    <View style={styles.viewStyle}>
+                        <Text style={styles.titleStyle}>Welcome to PokeSearch</Text>
+                        <Button
+                        block={true}
+                        tyle={styles.buttonStyle}
+                        onPress={()=>{}}>
+                            <Text style={styles.buttonText}>Start searching</Text>
+                        </Button>
+                    </View> 
+                </ImageBackground>
+            </View>
+        )
+    } 
+}
+
+
+const styles = {
+     
+    viewStyle: {
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    myBackground: {width: '100%', height: '100%'},
+    titleStyle: {
+      fontSize: 30, 
+      color: 'blue',
+      alignItems: 'center'
+  
+    },
+    buttonStyle: {margin: 10 },
+    buttonText: {color: 'white'}
+  };
+
+export default Landing;
